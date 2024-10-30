04fc2e1a1dbc9d79ac76f1acaf1fddd210c6dc83
yangwanqin <2140231689@qq.com>
2024-10-30 17:35:34 +0800

    123123

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println(12333);
         System.out.println(123);
         System.out.println("Hello world!");
     }
